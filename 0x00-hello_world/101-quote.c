b371a741e1f0eb1a56bc9af6e6f625698b58107d
Morender <morenderjama@gmail.com>
2023-06-15 05:22:17 -0700

    alx low level programing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 1 (Success)
+ * */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+			return(1)
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ * */
+int main(void)
+{
+	printf("The size of a char in bytes is %d \n
+		The size of an int in bytes is %d \n
+                The size of a float in bytes is %d \n
+                The size of a log int in bytes is %d \n
+                The size of a long long int in bytes is %d \n "
+		,sizeof(char),sizeof(int),sizeof(float),sizeof(long int),sizeof(long long int)
+
+	return(0)	
+}
